19baf573fe4ab74e2e558a70dc7c11339826be23
Alpha Chen <alpha.chen@gmail.com>
2015-12-09 22:19:55 -0800

    Day 10.0

---
+++ day_10.rb
@@ -0,0 +1,9 @@
+class Integer
+  def look_and_say
+    self.to_s.chars.slice_when {|a,b| a != b }.map {|i| "#{i.size}#{i[0]}" }.join.to_i
+  end
+end
+
+seed = 1113122113
+40.times { seed = seed.look_and_say }
+puts seed.to_s.size
