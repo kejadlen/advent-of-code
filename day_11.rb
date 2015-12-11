6567cc4e483d189051dba2ca7fb64420902e067e
Alpha Chen <alpha.chen@gmail.com>
2015-12-10 21:43:16 -0800

    Day 11.0

---
+++ day_11.rb
@@ -0,0 +1,17 @@
+class String
+  REQUIRED_SUBSTRING = (?a..?z).each_cons(3).map(&:join)
+  PAIRS = (?a..?z).map {|c| c*2 }
+
+  def password?
+    REQUIRED_SUBSTRING.any? {|s| self.include?(s) } &&
+      self !~ /[iol]/ &&
+      PAIRS.count {|p| self.include?(p) } >= 2
+  end
+end
+
+seed = "cqjxjnds"
+while true
+  seed = seed.succ
+  break if seed.password?
+end
+puts seed
