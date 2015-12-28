0eb87d8166daa394c66ebaef4629cfdf3c256f87
Alpha Chen <alpha.chen@gmail.com>
2015-12-27 22:20:11 -0800

    Move Day 1 into its own file/module

---
+++ rust/src/day_01.rs
@@ -0,0 +1,61 @@
+use std::io;
+use std::str::Chars;
+
+use day;
+
+pub struct Day01 {
+    input: String,
+}
+
+impl day::Day for Day01 {
+    fn new(input: String) -> Day01 {
+        Day01 { input: input }
+    }
+
+    fn solve(self) -> io::Result<i32> {
+        let elevator = Elevator::new(self.input);
+        Ok(1 + elevator.run().position(|f| f == -1).unwrap_or(0) as i32)
+    }
+}
+
+struct Elevator {
+    instructions: String,
+}
+
+impl<'a> Elevator {
+    fn new(input: String) -> Self {
+        Elevator { instructions: input }
+    }
+
+    fn run(self: &'a Self) -> ElevatorIterator<'a> {
+        ElevatorIterator {
+            chars: self.instructions.chars(),
+            floor: 0,
+        }
+    }
+}
+
+struct ElevatorIterator<'a> {
+    chars: Chars<'a>,
+    floor: i32,
+}
+
+impl<'a> Iterator for ElevatorIterator<'a> {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<i32> {
+        match self.chars.next() {
+            Some('(') => {
+                self.floor += 1;
+                Some(self.floor)
+            },
+            Some(')') => {
+                self.floor -= 1;
+                Some(self.floor)
+            },
+            _ => {
+                None
+            },
+        }
+    }
+}
