af554e7bc83d338d2d10de70f1860e586ccff564
Alpha Chen <alpha.chen@gmail.com>
2015-12-27 16:38:52 -0800

    Read some input strings

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,10 +1,25 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io;
+use std::path::PathBuf;
+
 extern crate advent_of_code;
 use advent_of_code::*;
 
+fn read_input(filename: &str) -> Result<String, io::Error> {
+    let mut path = PathBuf::from(".");
+    path.push("input");
+    path.push(filename);
+    path.set_extension("txt");
+
+    let mut f = try!(File::open(path));
+    let mut s = String::new();
+    try!(f.read_to_string(&mut s));
+    Ok(s)
+}
+
 fn main() {
-    let day = Day01::new("".to_string());
-    match day.solve() {
-        Ok(n) => println!("{}", n),
-        Err(e) => println!("{}", e),
-    }
+    let input = read_input("day_01").unwrap();
+    let day = Day01::new(input);
+    println!("{}", day.solve().unwrap());
 }
