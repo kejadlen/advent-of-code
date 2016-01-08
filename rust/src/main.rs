4b616449d8c1c10e49b3fe7064d8fd2765cf93a4
Alpha Chen <alpha.chen@gmail.com>
2016-01-08 08:07:01 -0800

    [rust] Use STDIN instead of reading from a file

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,25 +1,12 @@
-use std::fs::File;
 use std::io::prelude::*;
 use std::io;
-use std::path::PathBuf;
 
 extern crate advent_of_code;
 use advent_of_code::*;
 
-fn read_input(filename: &str) -> Result<String, io::Error> {
-    let mut path = PathBuf::from(".");
-    path.push("input");
-    path.push(filename);
-    path.set_extension("txt");
-
-    let mut f = try!(File::open(path));
-    let mut s = String::new();
-    try!(f.read_to_string(&mut s));
-    Ok(s)
-}
-
 fn main() {
-    let input = read_input("day_05").unwrap();
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).ok();
     let day = Day05::new(input);
     println!("{}", day.solve().unwrap());
 }
