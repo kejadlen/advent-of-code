7970c23d8883d6f1f95d4470c462af6d1c255e7e
Alpha Chen <alpha.chen@gmail.com>
2016-12-07 18:57:44 -0800

    [2016][rust][7.1]

--- 2016/rust/src/day_07.rs
+++ 2016/rust/src/day_07.rs
@@ -3,7 +3,7 @@ use errors::*;
 pub fn solve(input: &str) -> Result<String> {
   Ok(input.lines()
     .map(|x| IP7::new(x))
-    .filter(IP7::supports_tls)
+    .filter(IP7::supports_ssl)
     .count()
     .to_string())
 }
@@ -40,14 +40,35 @@ impl IP7 {
     }
   }
 
-  fn supports_tls(&self) -> bool {
+  pub fn supports_tls(&self) -> bool {
     self.supernets.iter().any(|x| Self::abba(x)) &&
     self.hypernets.iter().all(|x| !Self::abba(x))
+  }
+
+  fn supports_ssl(&self) -> bool {
+    let abas: Vec<String> =
+      self.supernets.iter().flat_map(|x| {
+        Self::abas(x).iter().map(|aba| {
+          let mut c = aba.chars();
+          let a = c.next().unwrap();
+          let b = c.next().unwrap();
+          format!("{}{}{}", b, a, b)
+        }).collect::<Vec<_>>()
+      }).collect();
+    self.hypernets.iter().any(|x| abas.iter().any(|y| x.contains(y)))
   }
 
   fn abba(s: &str) -> bool {
     let v: Vec<_> = s.chars().collect();
     v.windows(4).any(|w| (w[0] != w[1]) && (w[0] == w[3]) && (w[1] == w[2]))
+  }
+
+  fn abas(s: &str) -> Vec<String> {
+    let v: Vec<_> = s.chars().collect();
+    v.windows(3)
+      .filter(|w| (w[0] != w[1]) && (w[0] == w[2]))
+      .map(|w| w.iter().map(|&x| x).collect())
+      .collect()
   }
 }
 
@@ -74,5 +95,19 @@ mod tests {
     assert_eq!(IP7::new("abcd[bddb]xyyx").supports_tls(), false);
     assert_eq!(IP7::new("aaaa[qwer]tyui").supports_tls(), false);
     assert_eq!(IP7::new("ioxxoj[asdfgh]zxcvbn").supports_tls(), true);
+  }
+
+  #[test]
+  fn test_abas() {
+    assert_eq!(IP7::abas("zazbz"),
+               vec!["zaz".to_string(), "zbz".to_string()]);
+  }
+
+  #[test]
+  fn test_supports_ssl() {
+    assert!(IP7::new("aba[bab]xyz").supports_ssl());
+    assert!(!IP7::new("xyx[xyx]xyx").supports_ssl());
+    assert!(IP7::new("aaa[kek]eke").supports_ssl());
+    assert!(IP7::new("zazbz[bzb]cdb").supports_ssl());
   }
 }
