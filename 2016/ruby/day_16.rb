72d6fa5be9367b944aa67c7697bd40683f9bf364
Alpha Chen <alpha.chen@gmail.com>
2016-12-16 08:08:34 -0800

    [2016][ruby][16.0]

---
+++ 2016/ruby/day_16.rb
@@ -0,0 +1,24 @@
+def step(a)
+  b = a.reverse.tr('10', '01')
+  "#{a}0#{b}"
+end
+
+def checksum(data)
+  checksum = data.chars
+  loop do
+    checksum = checksum.each_slice(2).map {|a,b| (a == b) ??1:?0 }
+    break if checksum.length.odd?
+  end
+  checksum.join
+end
+
+seed = '10001110011110000'
+target_length = 272
+
+state = seed
+while state.length < target_length
+  state = step(state)
+end
+
+data = state[0...target_length]
+puts checksum(data)
