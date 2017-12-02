26ac38d39fcd798ce1f4afa61e76c4c7edddb37d
Alpha Chen <alpha.chen@gmail.com>
2017-12-02 11:30:09 -0800

    [2017][rust][1.0] refactor

--- 2017/rust/src/day_01.rs
+++ 2017/rust/src/day_01.rs
@@ -1,20 +1,21 @@
 use failure::*;
 
 pub fn solve(input: &str) -> Result<String, Error> {
-    let mut input: Vec<u32> = input
+    let input: Vec<u32> = input
         .trim()
         .chars()
         .map(|x| x.to_digit(10).ok_or_else(|| format_err!("")))
         .collect::<Result<_, _>>()?;
 
     // 1
-    let last = input[input.len() - 1];
-    input.push(last);
+    let offset = 1;
+    let offset_iter = input.iter().skip(offset).cycle().take(input.len());
     Ok(
         input
-            .windows(2)
-            .filter(|&a| a[0] == a[1])
-            .map(|a| a[0])
+            .iter()
+            .zip(offset_iter)
+            .filter(|&a| a.0 == a.1)
+            .map(|a| a.0)
             .sum::<u32>()
             .to_string(),
     )
