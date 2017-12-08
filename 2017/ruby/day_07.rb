d447e45d1197485e274d04555fffc5d7d09c469f
Alpha Chen <alpha.chen@gmail.com>
2017-12-08 13:09:13 -0800

    [2017][ruby][7.x] move back to ARGF

--- 2017/ruby/day_07.rb
+++ 2017/ruby/day_07.rb
@@ -1,7 +1,7 @@
 Program = Struct.new(:name, :weight, :disc)
 
 programs = {}
-File.read(File.expand_path("../input/day_07.txt")).strip.split("\n").each do |line|
+ARGF.read.strip.split("\n").each do |line|
   name, weight, above = line.scan(/(.+) \((\d+)\)(?: -> (.+))?/)[0]
   programs[name] = Program.new(name, weight.to_i, (above || "").split(", "))
 end
