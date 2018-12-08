6b086acd5b5996b2e8299a10b63d8a9bf8fd4108
Alpha Chen <alpha.chen@gmail.com>
2018-12-07 21:09:17 -0800

    [2018][ruby][08] speedrun

---
+++ 2018/ruby/day_08.rb
@@ -0,0 +1,34 @@
+raw = ARGF.read.chomp.split(/\s+/).map(&:to_i)
+
+class Node
+  def self.from(raw)
+    child_count = raw.shift
+    metadata_count = raw.shift
+
+    children = child_count.times.map { Node.from(raw) }
+    metadata = metadata_count.times.map { raw.shift }
+
+    Node.new(children, metadata)
+  end
+
+  def initialize(children, metadata)
+    @children, @metadata = children, metadata
+  end
+
+  def checksum
+    @metadata.sum + @children.map(&:checksum).sum
+  end
+
+  def value
+    if @children.empty?
+      @metadata.sum
+    else
+      @metadata.flat_map {|index|
+        @children.fetch(index-1, [])
+      }.map(&:value).sum
+    end
+  end
+end
+
+root = Node.from(raw)
+p root.value
