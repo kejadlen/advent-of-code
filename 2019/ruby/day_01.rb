b15f5ee2a1fbe80ad6a8dde631d11f28794849b0
Alpha Chen <alpha@kejadlen.dev>
2019-12-01 20:40:52 -0800

    [2019][ruby][1.2]

---
+++ 2019/ruby/day_01.rb
@@ -0,0 +1,15 @@
+# puts ARGF.read.lines.map(&:to_i).map {|mass|
+#   (mass / 3.0).floor - 2
+# }.sum
+
+fuel = ->(mass) {
+  total = 0
+  loop do
+    mass = (mass / 3.0).floor - 2
+    break if mass <= 0
+    total += mass
+  end
+  total
+}
+
+puts ARGF.read.lines.map(&:to_i).map(&fuel).sum
