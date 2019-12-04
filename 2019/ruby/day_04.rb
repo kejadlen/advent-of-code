a3d275c27d75dcb7f8b14239dff8d682f7504664
Alpha Chen <alpha@kejadlen.dev>
2019-12-03 21:20:50 -0800

    [2019][ruby][4.x]

---
+++ 2019/ruby/day_04.rb
@@ -0,0 +1,5 @@
+puts (236491..713787).map(&:to_s).count {|password|
+  matching_digits = password.scan(/(.)\1/)
+
+  !matching_digits.empty? && matching_digits.flatten.any? {|d| !password.include?(d * 3) } && password.chars.each_cons(2).all? {|a,b| a <= b }
+}
