a9f664f3f3b47a8511ed52a5142c6f503909cd9f
Alpha Chen <alpha@kejadlen.dev>
2020-12-02 21:12:56 -0800

    [2020][ruby][2.x]

--- 2020/ruby/day_02.rb
+++ 2020/ruby/day_02.rb
@@ -1,4 +1,5 @@
-p ARGF.read.scan(/(\d+)-(\d+)\s*(\w):\s*(\w+)/)
+day1 = proc {|min,max,char,pass| (min..max).cover?(pass.count(char)) }
+day2 = proc {|p1,p2,char,pass| [pass[p1-1], pass[p2-1]].count(char) == 1 }
+puts ARGF.read.scan(/^(\d+)-(\d+)\s*(\w):\s*(\w+)$/)
   .map {|a,b,c,d| [a.to_i, b.to_i, c, d] }
-  # .count {|min,max,char,pass| (min..max).cover?(pass.count(char)) }
-  .count {|p1,p2,char,pass| [pass[p1-1], pass[p2-1]].tally[char] == 1 }
+  .count(&day2)
