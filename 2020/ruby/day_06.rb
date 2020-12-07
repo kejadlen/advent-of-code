e49d6c4bef1c2d582da5d9515c6f7a5cb33cfd76
Alpha Chen <alpha@kejadlen.dev>
2020-12-06 20:57:39 -0800

    [2020][ruby][6.x] refactor

--- 2020/ruby/day_06.rb
+++ 2020/ruby/day_06.rb
@@ -1,6 +1,6 @@
-# p ARGF.read.split("\n\n").map {|g| g.gsub("\n", "").chars.uniq.count }.sum
 p ARGF.read.split("\n\n").map {|group|
-  people = group.split("\n").map(&:chars)
-  first = people.shift
-  first.count {|answer| people.all? {|answers| answers.include?(answer) }}
-}.sum
+  group.split("\n").map(&:chars)
+}.map {|group|
+  # group.inject(&:|)
+  group.inject(&:&)
+}.map(&:size).sum
