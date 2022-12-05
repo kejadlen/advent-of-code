c92d5a4346c27d48aaf1db39b8248cb621034968
Alpha Chen <alpha@kejadlen.dev>
2022-12-04 21:16:27 -0800

    [2022][ruby][5.x] refactor

--- 2022/ruby/day_05.rb
+++ 2022/ruby/day_05.rb
@@ -4,8 +4,8 @@ setup = setup
   .lines(chomp: true)
   .map(&:chars)
   .transpose
-  .select { _1.last =~ /\d/ }
-  .map { _1[0..-2].join.strip.chars }
+  .map {|col| col.select { _1 =~ /[A-Z]/ }}
+  .reject(&:empty?)
 
 moves = moves.scan(/move (\d+) from (\d+) to (\d+)/).map { _1.map(&:to_i) }
 moves.each do |n,from,to|
